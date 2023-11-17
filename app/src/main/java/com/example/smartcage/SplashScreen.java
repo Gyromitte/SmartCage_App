465e7a47c9e1ffa6b5cbcda8a80d0d969c55e569
Gyromitte <gyromitte@gmail.com>
2023-11-17 11:00:34 -0600

    Comportamiento de Splash Scren Se agrego un intent con un delay de 4 segundos para abrir el main activity

--- app/src/main/java/com/example/smartcage/SplashScreen.java
+++ app/src/main/java/com/example/smartcage/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.smartcage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Intent para ir al main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                // Cerrar splash screen
+                finish();
+            }
+        }, 4000); //Delay de 4 segunods
     }
 }

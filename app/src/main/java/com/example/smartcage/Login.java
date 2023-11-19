f4aa6c7662027da7d3239e3a1f3bef349a75e921
Allixdx <allison.sanroman@outlook.com>
2023-11-18 23:45:55 -0600

    Le di mejor fomato a login

--- app/src/main/java/com/example/smartcage/Login.java
+++ app/src/main/java/com/example/smartcage/Login.java
@@ -2,13 +2,28 @@ package com.example.smartcage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    private Button account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        account = findViewById(R.id.account);
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this, RegisteredAccount.class);
+                startActivity(i);
+            }
+        });
     }
 }

7e50415cf1944d786b0f8c033bf92bd5dfb58222
Allixdx <allison.sanroman@outlook.com>
2023-12-03 23:40:19 -0600

    Internet permission y dependencias retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("com.square.retrofit2:retrofit:2.9.0")
+    implementation("com.square.retrofit2:converter-gson:2.9.0")
     implementation("com.airbnb.android:lottie:6.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
